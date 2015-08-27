68f11abe8582e87fdb601f3061f8d5801fe09c73
tokinonagare <timemahou@gmail.com>
2015-08-27 17:54:21 +0800

    Build IntentService when app Start and activity be awaked

--- app/src/main/java/tokinonagare/intenttest/BuckysIntentService.java
+++ app/src/main/java/tokinonagare/intenttest/BuckysIntentService.java
@@ -1,7 +1,20 @@
 package tokinonagare.intenttest;
 
-/**
- * Created by tokinonagare on 8/27/15.
- */
-public class BuckysIntentService {
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class BuckysIntentService extends IntentService {
+
+    private static final String TAG = "tokinonagare.intenttest";
+
+    public BuckysIntentService() {
+        super("BuckysIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //This is what the service does
+        Log.i(TAG, "The Service has now started!");
+    }
 }
